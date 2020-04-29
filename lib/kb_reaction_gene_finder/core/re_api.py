79375003e54a7785d93f3d4b16febd61412720e9
Jay R Bolton <jayrbolton@gmail.com>
2020-04-29 09:54:03 -0700

    Bugfix: fix url for RE API and fix param type

--- lib/kb_reaction_gene_finder/core/re_api.py
+++ lib/kb_reaction_gene_finder/core/re_api.py
@@ -10,7 +10,7 @@ class RE_API:
         self.re_url = re_url
         self.token = token
 
-    def _call_re(self, endpoint="/api/query_results/", params=None, data=None):
+    def _call_re(self, endpoint="/api/v1/query_results/", params=None, data=None):
         header = {"Authorization": self.token}
         logging.info(f"Calling RE_API with query data: {pformat(data)}")
         ret = requests.post(self.re_url+endpoint, data, params=params, headers=header)
@@ -27,10 +27,17 @@ class RE_API:
                 OPTIONS {uniqueVertices: "global", bfs: true}
                 FILTER !e || e.sf_similarity >= @sf_sim
                 FILTER !e || e.df_similarity >= @df_sim
-                RETURN {id: v._id, key: v._key, name: v.name, definition: v.definition, "structural similarity": e.sf_similarity, "difference similarity": e.df_similarity}
+                RETURN {
+                    id: v._id,
+                    key: v._key,
+                    name: v.name,
+                    definition: v.definition,
+                    "structural similarity": e.sf_similarity,
+                    "difference similarity": e.df_similarity
+                }
         )
         LET rxn_ids = rxns[*].id
-        
+
         LET rxn_gene_links = (
             FOR e in rxn_reaction_within_complex
                 FILTER e._from in rxn_ids
@@ -42,16 +49,21 @@ class RE_API:
                 COLLECT rxn_id = e._from INTO groups KEEP linked_gene_ids
                 RETURN {rxn_id: rxn_id, linked_gene_ids: UNIQUE(FLATTEN(groups[*].linked_gene_ids))}
         )
-        
+
         LET gene_ids = UNIQUE(FLATTEN(rxn_gene_links[*].linked_gene_ids))
-        
+
         LET genes = (
             FOR g in ncbi_gene
                FILTER g._key IN gene_ids
-               RETURN {key: g._key, product: g.product, function: CONCAT_SEPARATOR(', ', g.functions), sequence: g.protein_translation}
+               RETURN {
+                   key: g._key,
+                   product: g.product,
+                   function: CONCAT_SEPARATOR(', ', g.functions),
+                   sequence: g.protein_translation
+               }
         )
         LET missing = MINUS(gene_ids, genes[*].key)
-        
+
         RETURN {rxns: rxns, rxn_gene_links: rxn_gene_links, genes: genes, missing_genes: missing}
         """
         if not rid.startswith('rxn_reaction/'):
@@ -64,17 +76,16 @@ class RE_API:
         logging.info(f"Found {len(ret['results'][0]['genes'])} related sequences")
         return ret['results'][0]
 
-    def get_related_sequences(self, rid, sf_sim=1, df_sim=1, exclude_self=0, retries=2):
+    def get_related_sequences(self, rid, sf_sim=1, df_sim=1, exclude_self=False, retries=2):
         if not rid.startswith('rxn_reaction/'):
             rid = 'rxn_reaction/' + rid
         body = json.dumps({'rid': rid, 'sf_sim': sf_sim, 'df_sim': df_sim,
                            'exclude_self': exclude_self})
         ret = self._call_re(params={'view': "list_genes_for_similar_reactions"}, data=body)
+        logging.info(f"RE API result:\n{json.dumps(ret, indent=2)}")
         if "error" in ret:
             if retries:
                 logging.warning("Arango Query failed. Retrying")
                 return self.get_related_sequences(rid, sf_sim, df_sim, exclude_self, retries-1)
             raise RuntimeError(f"{ret['error']}: {ret.get('arango_message', '')}")
-        logging.info(f"Found {len(ret['results'][0]['genes'])} related sequences")
         return ret['results'][0]
-
